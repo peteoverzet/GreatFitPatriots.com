ba05d971ed2e3e2970d0d92875e6702fad3e5aee
Justin Kenyon <kenyonj@gmail.com>
2019-02-21 22:45:29 -0500

    return random row in responses

--- js/randomLine.js
+++ js/randomLine.js
@@ -1,7 +1,8 @@
 (function () {
   $(document).ready(function () {
     $.get('responses.txt', function(data) {
-      console.log(data);
+      var rows = data.split('\n');
+      rows[Math.floor(Math.random()*rows.length)];
     }, 'text');
   });
 }());
