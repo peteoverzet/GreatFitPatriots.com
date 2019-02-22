4b654af27b6bd5950ff2b932d82b2d4e57dafb86
Justin Kenyon <kenyonj@gmail.com>
2019-02-21 22:59:27 -0500

    define correctly

--- js/randomLine.js
+++ js/randomLine.js
@@ -1,15 +1,16 @@
 (function () {
   $(document).ready(function () {
-    var getResponse = function () {
-      $.get('responses.txt', function(data) {
-        var rows = data.split('\n');
-        return rows[Math.floor(Math.random()*rows.length)];
-      }, 'text');
-    };
-
-    var showResponse = function () {
-      var response = getResponse();
-      $("#response").innerHtml(response);
-    };
   });
 }());
+
+function getResponse() {
+  $.get('responses.txt', function(data) {
+    var rows = data.split('\n');
+    return rows[Math.floor(Math.random()*rows.length)];
+  }, 'text');
+};
+
+function showResponse() {
+  var response = getResponse();
+  $("#response").innerHtml(response);
+};
